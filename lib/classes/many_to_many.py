98f32cfc662d3be0d64f893a40a73dfddb0af219
IanNjunge <ian.njunge@student.moringaschool.com>
2025-08-27 18:42:15 +0300

    Saves changes

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,11 +1,30 @@
 class Author:
     def __init__(self, name):
-        if not isinstance(name, str) or not (1 <= len(name) <= 50): raise TypeError("name must be a string between 1 and 50 characters")
+        if not isinstance(name, str) or not (1 <= len(name) <= 50): 
+            raise TypeError("name must be a string between 1 and 50 characters")
         self._name = name
         
     @property   
     def name(self):
         return self._name
+    
+    #Articles by author
+    def articles(self):
+        return [article for article in Article.all if article.author is self]
+    
+    #Magazines author is a part of
+    @property
+    def magazines(self):
+        return list({article.magazine for article in self.articles()})
+    
+    #new article for author
+    def add_articles(self, magazine, title):
+        return Article(self, magazine, title)
+    
+    #topics author wrote about
+    def topic_areas(self):
+        return list({article.magazine.category for article in self.articles()})
+    
     
 class Magazine:
     def __init__(self, name, category):
@@ -32,17 +51,37 @@ class Magazine:
             raise TypeError("category must be a non-empty string")
         self._category = value
         
-
+    #all the articles in magazine   
+    def articles(self):
+        return [article for article in Article.all if article.magazine is self]
+    
+    #titles of all articles of magazine
+    def article_titles(self):
+        return [article.title for article in self.articles()]
+    
+    #authors of magazine
+    def contributors(self):
+        return list ({article.author for article in self.articles()})
+    
+    #authors of more than 2 articles
+    def contributing_authors(self):
+        authors = [article.author for article in self.articles()]
+    
+    
 class Article:
-    all = [] #store every article created
+    #stores every article
+    all = [] 
     
     def __init__(self, author, magazine, title):
+        if not isinstance(title, str)or not(5 <=len(title) <= 50): 
+            raise TypeError("title must be a string between 5 to 50 characters")
+        if not isinstance(magazine, Magazine): 
+            raise TypeError("magazine must be an instance of Magazine")
+        if not isinstance(title, str) or not (5 <= len(title) <=50): 
+            raise TypeError("title must be string between 5 and 50 characters")
+        self._title = title
         self._author = author  #private attributes
         self._magazine = magazine
-        
-
-        if not isinstance(title, str)or not(5 <=len(title) <= 50): raise TypeError("title must be a string between 5 to 50 characters")
-        self._title = title
         
         Article.all.append(self)    
         
@@ -50,20 +89,23 @@ class Article:
     def author(self):
         return self._author
     
-    @author.setter
-    def author(self, value):
-        if not isinstance(value, Author): raise TypeError("author must be an instance of Author")
-        self._author = value
-    
+   
     @property
     def magazine(self):
         return self._magazine
     
     @magazine.setter
     def magazine(self, value):
-        if not isinstance(value, Magazine): raise TypeError("magazine must be an instance of Magazine")
+        if not isinstance(value, Magazine): 
+            raise TypeError("magazine must be an instance of Magazine")
         self._magazine = value
          
+    @author.setter
+    def author(self, value):
+        if not isinstance(value, Author):
+            raise TypeError("author must be an instance of Author")
+        self._author = value
+             
     @property
     def title(self):
         return self._title
